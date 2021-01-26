d1c96a3f9b5110552d129d038f8f374f2e74e45c
orsolon <56239735+orsolon@users.noreply.github.com>
2021-01-26 12:05:33 -0300

    diretivas ngIf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    FooterComponent
+    FooterComponent,
+    DiretivaNgifComponent
   ],
   imports: [
     BrowserModule
